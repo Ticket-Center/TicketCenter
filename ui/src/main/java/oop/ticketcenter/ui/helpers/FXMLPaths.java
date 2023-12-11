5b00b59142592eb8d6a1704f8210880d1c62ccfd
p.bodurova <p.bodurova@tinqin.com>
2023-12-11 23:37:57 +0200

    added skeleton for events and tickets operation

--- ui/src/main/java/oop/ticketcenter/ui/helpers/FXMLPaths.java
+++ ui/src/main/java/oop/ticketcenter/ui/helpers/FXMLPaths.java
@@ -3,7 +3,10 @@ package oop.ticketcenter.ui.helpers;
 public enum FXMLPaths {
     LOGIN_FORM("/fxmls/login.fxml"),
     FORGOT_PASS_FORM("/fxmls/forgotpassword.fxml"),
-    REGISTER_NEW_USER("/fxmls/registernewuser.fxml");
+    REGISTER_NEW_USER("/fxmls/registernewuser.fxml"),
+    NAVIGATION_USER("/fxmls/navigation.fxml"),
+    NAVIGATION_ADMIN("/fxmls/navigationAdmin.fxml"),
+    HOME_PAGE("/fxmls/homePage.fxml");
 
     private String path;
 
