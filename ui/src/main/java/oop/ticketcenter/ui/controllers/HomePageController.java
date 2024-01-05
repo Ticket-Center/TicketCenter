3ffd49d0e0c78252c6fe7e5825da38a474c3ca4a
RositsaSlavova <slavova02@gmail.com>
2024-01-05 15:45:52 +0200

    almost done tickets to HomePageController

--- ui/src/main/java/oop/ticketcenter/ui/controllers/HomePageController.java
+++ ui/src/main/java/oop/ticketcenter/ui/controllers/HomePageController.java
@@ -13,17 +13,19 @@ import oop.ticketcenter.core.interfaces.users.logout.Logout;
 import oop.ticketcenter.core.interfaces.users.logout.LogoutInput;
 import oop.ticketcenter.core.services.helpers.ActiveUserSingleton;
 import oop.ticketcenter.core.services.helpers.GetEvents;
+import oop.ticketcenter.core.services.helpers.GetTicketInfo;
 import oop.ticketcenter.persistence.entities.Event;
+import oop.ticketcenter.persistence.entities.EventSeatPrice;
 import oop.ticketcenter.persistence.enums.Roles;
 import oop.ticketcenter.ui.helpers.FXMLPaths;
 import oop.ticketcenter.ui.helpers.SceneSwitcher;
-import oop.ticketcenter.ui.model.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class HomePageController {
@@ -58,8 +60,6 @@ public class HomePageController {
     @FXML
     private GridPane ticketGrid;
 
-    private List<Ticket> tickets;
-
     @FXML
     private VBox vboxTicket;
 
@@ -69,31 +69,51 @@ public class HomePageController {
     @Autowired
     private GetEvents getEvents;
 
+    @Autowired
+    private GetTicketInfo getTicketInfo;
+
     @FXML
     public void initialize() {
+        visibilityForButtons();
+        updateTicketGrid();
+    }
+
+    private void visibilityForButtons(){
         if(!ActiveUserSingleton.getInstance().getUserRole().equals(Roles.ADMIN)){
             btnUsers.setVisible(false);
         }
-        //if(!(ActiveUserSingleton.getInstance().getUserRole().equals(Roles.ADMIN) || ActiveUserSingleton.getInstance().getUserRole().equals(Roles.ORGANIZER)||ActiveUserSingleton.getInstance().getUserRole().equals(Roles.OWNER) || ActiveUserSingleton.getInstance().getUserRole().equals(Roles.SELLER))){
         if(ActiveUserSingleton.getInstance().getUserRole().equals(Roles.CLIENT)){
             btnCreate.setVisible(false);
             btnEdit.setVisible(false);
             btnDelete.setVisible(false);
         }
         lbName.setText(ActiveUserSingleton.getInstance().getUsername());
+    }
 
-        //Set<Event> events = getEvents.getEvents();
-        Set<Event> events =getEvents.fetchAllEvents();
+    private void updateTicketGrid(){
+        Set<Event> events = switch (ActiveUserSingleton.getInstance().getUserRole()) {
+            case CLIENT -> getEvents.fetchAllEvents();
+            case OWNER -> filterEventsForOwner();
+            case SELLER -> filterEventsForSeller();
+            case ORGANIZER -> filterEventsForOrganizer();
+            default -> Collections.emptySet();
+        };
+
+        Set<EventSeatPrice> ticketsInfo=getTicketInfo.fetchAllEventSeatPrice();
 
         int row = 1;
         try {
             for (Event event : events) {
                 FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("/fxmls/ticket.fxml"));
+                fxmlLoader.setLocation(getClass().getResource(FXMLPaths.TICKET.getPath()));
                 BorderPane box = fxmlLoader.load();
                 TicketController ticketController = fxmlLoader.getController();
 
-                ticketController.setData(event);
+                Set<EventSeatPrice> filteredTicketInfo = ticketsInfo.stream()
+                        .filter(ticket -> ticket.getEvent().getId().equals(event.getId()))
+                        .collect(Collectors.toSet());
+
+                ticketController.setData(event, filteredTicketInfo);
 
                 ticketGrid.add(box, 0, row++);
                 GridPane.setMargin(box, new Insets(10));
@@ -101,43 +121,29 @@ public class HomePageController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        /*tickets=new ArrayList<>(tickets());
-        int row=1;
-        try{
-            for (int i=0; i<tickets.size();i++){
-                FXMLLoader fxmlLoader=new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("/fxmls/ticket.fxml"));
-                BorderPane box = fxmlLoader.load();
-                TicketController ticketController=fxmlLoader.getController();
-                ticketController.setData(tickets.get(i));
-
-                ticketGrid.add(box,0,row++);
-                GridPane.setMargin(box, new Insets(10));
-            }
-        }catch(IOException e){
-            e.printStackTrace();
-        }*/
+    }
+    private Set<Event> filterEventsForOwner() {
+        return getEvents.fetchAllEvents().stream()
+                .filter(event -> event.getEventOwner().getUsername().equals(ActiveUserSingleton.getInstance().getUsername()))
+                .collect(Collectors.toSet());
     }
 
+    private Set<Event> filterEventsForSeller() {
+        return Collections.emptySet();
+        /*return getEvents.fetchAllEvents().stream()
+                .filter(event -> event.getEventSeller().getUsername().equals(ActiveUserSingleton.getInstance().getUsername()))
+                .collect(Collectors.toSet());*/
+    }
+
+    private Set<Event> filterEventsForOrganizer() {
+        return getEvents.fetchAllEvents().stream()
+                .filter(event -> event.getEventOrganizator().getUsername().equals(ActiveUserSingleton.getInstance().getUsername()))
+                .collect(Collectors.toSet());
+    }
 
     @FXML
     private void logoutuser() throws IOException {
         logout.process(new LogoutInput());
         SceneSwitcher.switchScene((Stage) btnEvents.getScene().getWindow() , FXMLPaths.LOGIN_FORM.getPath());
     }
-
-    /*private List<Ticket> tickets(){
-        List<Ticket> ticketList=new ArrayList<>();
-
-        for (int i=0; i<10;i++) {
-            Ticket ticket = new Ticket();
-            ticket.setTitle("proba");
-            ticket.setGenre("genre");
-            ticket.setType("type");
-            ticket.setDate("28.12.2023");
-            ticketList.add(ticket);
-        }
-        return ticketList;
-    }*/
 }
